c73cca34f74ac6791ea2911f2348679df7990e74
Administrator <pedrootero@unifeg.edu.br>
2022-03-29 16:34:40 -0300

    criando internet_gateway

--- modules/network/internet.tf
+++ modules/network/internet.tf
@@ -24,3 +24,15 @@ resource "aws_route" "public_internet_access" {
     gateway_id = aws_internet_gateway.gw.id
   
 }
+
+resource "aws_route_table_association" "public_1a" {
+    subnet_id = aws_subnet.public_subnet_1a.id
+    route_table_id = aws_route_table.igw_route_table.id
+  
+}
+
+resource "aws_route_table_association" "public_1c" {
+    subnet_id = aws_subnet.public_subnet_1c.id
+    route_table_id = aws_route_table.igw_route_table.id
+  
+}
